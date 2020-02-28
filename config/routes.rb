596302c59af8fe9b3681bfcc7d15c4a05320384f
chudai8141 <chu.dai.kimukimu.0420@gmail.com>
2020-02-29 00:01:30 +0900

    first setting url

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+
+  get 'posts/index' => "posts#index"
   get '/' => "home#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/about' => "home#about"
+  
 end
